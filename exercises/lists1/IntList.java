20fab83393c4f3176f8c06bb55ff43e0c8c1c4a5
kanyu <kanyuds@gmail.com>
2018-12-30 05:58:30 +0700

    finish exercise 1

--- exercises/lists1/IntList.java
+++ exercises/lists1/IntList.java
@@ -8,20 +8,47 @@ public class IntList {
 	}
 
 	/** Return the size of the list using... recursion! */
-	public int size() {
-		return 0;
-	}
+	public int size(){
+    if(this.rest == null){
+      return 1;
+    }else{
+      return 1 + this.rest.size();
+    }
+  }
 
 	/** Return the size of the list using no recursion! */
 	public int iterativeSize() {
-		return 0;
-	}
+		IntList L = this;
+    int size = 1;
+    while(L.rest != null){
+      size += 1;
+      L = L.rest;
+    }
+    return size;
+  }
 
 	/** Returns the ith value in this list.*/
 	public int get(int i) {
-		return 0;
-	}
-
+		if(i == 0){
+      return this.first;
+    }else{
+      return this.rest.get(i - 1);
+    }
+  }
+	// return number at index i iterative method
+  public int getIter(int i){
+    int index = 0;
+    IntList L = this;
+    if(index == i){
+      return L.first;
+    }else{
+      while ((index < i) && (L != null)){
+        index += 1;
+        L = L.rest;
+      }
+      return L.first;
+    }
+  }
 	public static void main(String[] args) {
 		IntList L = new IntList(15, null);
 		L = new IntList(10, L);
@@ -29,4 +56,4 @@ public class IntList {
 
 		System.out.println(L.iterativeSize());
 	}
-} 
+}
